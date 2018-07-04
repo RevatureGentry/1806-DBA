3a922bdc548eaedb41abe644065f99a6d2a897d9
DailyDataMiner <40675349+DailyDataMiner@users.noreply.github.com>
2018-07-04 14:27:30 -0400

    Update reverse.sh

--- Reverse/reverse.sh
+++ Reverse/reverse.sh
@@ -7,9 +7,9 @@ length=${#copy}
 for((int=$length-1;int>=0;int--));
 do
 	rev="$rev${copy:$int:1}";
-#copy is the string it will be indexing
-#With length-1, it will take the end of the input and add it to the variable rev.
-#$rev$~ will concatenate until int < o
+	#copy is the string it will be indexing
+	#With length-1, it will take the end of the input and add it to the variable rev.
+	#$rev$~ will concatenate until int < o
 done
 
 echo "The reverse of $input is $rev."
