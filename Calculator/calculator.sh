aff42cb7aa704955769499a4cf24e58863aa39af
DailyDataMiner <benjaminmartin@theitguy.me>
2018-07-04 14:14:08 -0400

    Removed older calculator script and added the final version with data validation.

---
+++ Calculator/calculator.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+while true
+do
+	n1=""
+	while [[ ! $n1 =~ ^[0-9]+$ ]]
+	do
+    		echo Please enter the first number you wish to calculate.
+    		read n1
+	done
+	#Data validation so only a number is accepted.
+
+	n2=""
+	while [[ ! $n2 =~ ^[0-9]+$ ]]
+	do
+    		echo Please enter the second number you wish to calculate.
+  		read n2
+	done
+	#Data validation so only a number is accepted.
+
+	echo "Which operation would you like to use? "
+
+	select ans in add subtract multiply divide; do
+		case $ans in
+			add) op='+'
+			break ;;
+			subtract) op='-'
+			break ;;
+			multiply) op='*'
+			break ;;
+			divide) op='/'
+			break ;;
+			*) echo "invalid response"
+			;;
+		esac
+	done
+	#Presents the four options for arithmetic while looping if any other input is given.
+	let ans="$n1$op$n2"
+	#Does the calculation from the inputs
+	printf "%s %s %s = %s\n\n" "$n1" "$op" "$n2" "$ans"
+	#Fancy formatting to present the calculation
+done
+#While True will allow it to loop once the calculation is complete
