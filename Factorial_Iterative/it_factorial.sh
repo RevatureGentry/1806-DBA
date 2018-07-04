4e6f9d92c7decce33db0365f1bcd4422574be045
DailyDataMiner <benjaminmartin@theitguy.me>
2018-07-04 13:14:59 -0400

    Final version of Factorial Script with Data Validation

---
+++ Factorial_Iterative/it_factorial.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+count=""
+while [[ ! $count =~ ^[0-9]+$ ]]
+do
+    echo Please enter your a number
+    read count
+done
+#This is to ensure the data the user inputs is only numbers.
+
+input=$count
+#Need to store their answer for the end
+number=1
+#What will hold the total
+
+while [ $count -gt 0 ]
+do
+	number=$(( $number * $count ))
+	count=$(( $count - 1 ))
+done
+#Until the number given by the user (count) is < 1, it will multiple the count by number to modify number with the newest 
+#calculation
+echo "The factorial of $input is $number."
