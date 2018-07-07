a25d924f66e10fea3a526e193869b7647613a03d
andre-rowell <andrerowell@outlook.com>
2018-07-07 00:19:31 -0400

    Birthday script

--- asgn1/birthday.sh
+++ asgn1/birthday.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 
-echo 'Please enter a birthday:'
+echo 'Please enter a birthday in YYYYMMDD:'
 read birthday
+b=`date -d "$birthday" "+%s"`
+d=`date "+%s"`
+s=$(( d - b))
+echo "$r"
+years=$(( s / 31556926))
+days=$(( s / 86400))
+hours=$(( s / 3600))
 
-echo $((date -d "2010-06-01" "+%s")) - $(date -d "2010-05-15" "+%s") ) / 86400))
+echo 'Years: ' $years
+echo 'Days: ' $days
+echo 'Hours: ' $hours
+echo 'Seconds: ' $s
