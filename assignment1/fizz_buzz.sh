12efe6bf7d1c1e9f8db7a225fa1a670705bdef7a
islamhafairi <islamhafairi@gmail.com>
2018-07-07 13:52:30 -0400

    Fizz Buzz w/ error output

--- assignment1/fizz_buzz.sh
+++ assignment1/fizz_buzz.sh
@@ -1,27 +1,29 @@
 #!/bin/bash
-
+# Set date for error output
 DATE=$( date +"%d/%m/%Y %H:%M:%S %p")
+# Error Output if user inserts argument
 if [ $# -gt 0 ]
 then 
 	echo "Do not enter any arguments"
 	"$DATE Do not enter any arguments." 2>>bash_assignment_error_log.txt
 	exit
 fi
-
+# Set array for numbers 1 to 100
 ARRAY=({1..100})
-
-for i in "${ARRAY[@]}";
+# Use for loop to run through the array
+for num in "${ARRAY[@]}";
 do
-	if { ( (("$i" % 3 == 0)) && (("$i" % 5 == 0)) ) }
+# Set conditions to find remainder = 0 & print corresponding output
+	if { ( (("$num" % 3 == 0)) && (("$num" % 5 == 0)) ) }
 	then
-		echo "$i" 'FizzBuzz'
-	elif (( "$i" % 3 == 0 ))
+		echo "$num" 'FizzBuzz'
+	elif (( "$num" % 3 == 0 ))
 	then
-		echo "$i" 'Fizz'
-	elif (( "$i" % 5 == 0 ))
+		echo "$num" 'Fizz'
+	elif (( "$num" % 5 == 0 ))
 	then 
-		echo "$i" 'Buzz'
+		echo "$num" 'Buzz'
 	else
-		echo "$i"
+		echo "$num"
 	fi
 done
