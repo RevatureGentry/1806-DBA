4e3c534209538a576d7a34b340e6011e591b6e2e
andre-rowell <andrerowell@outlook.com>
2018-07-08 15:54:13 -0400

    Forgot to comment code

--- asgn1/birthday.sh
+++ asgn1/birthday.sh
@@ -2,14 +2,19 @@
 
 echo 'Please enter a birthday in YYYYMMDD:'
 read birthday
+#converts birthday into seconds
 b=`date -d "$birthday" "+%s"`
+#grabs system time
 d=`date "+%s"`
+#difference between the 2
 s=$(( d - b))
-echo "$r"
+
+#converts seconds to years, days, and hours
 years=$(( s / 31556926))
 days=$(( s / 86400))
 hours=$(( s / 3600))
 
+#output
 echo 'Years: ' $years
 echo 'Days: ' $days
 echo 'Hours: ' $hours
