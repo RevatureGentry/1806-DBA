ac2e7e28eb56d23c8ec9a06e309396458d669f21
RevatureGentry <william.gentry@revature.com>
2018-07-16 10:27:38 -0400

    Monday morning Java examples

---
+++ WeekThree/in-class-java/HelloWorld.java
@@ -0,0 +1,25 @@
+// This is a comment
+/*
+*
+* This is a multiline comment 
+* 
+*
+*/
+
+// Public class must be same name as file name
+public class HelloWorld {
+	// Recall that "the main method" is the starting point
+	// For any application
+	public static void main(String[] bananas) {
+		System.out.println("Hello World!");
+	}
+}
+
+
+
+
+
+
+
+
+
