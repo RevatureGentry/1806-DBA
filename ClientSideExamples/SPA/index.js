d2458eb47a598e0f84cae71a2c8d529337ff791c
RevatureGentry <william.gentry@revature.com>
2018-07-27 16:00:50 -0400

    IMDB Example

--- ClientSideExamples/SPA/index.js
+++ ClientSideExamples/SPA/index.js
@@ -1,7 +1,12 @@
+const apiKey = "f445b7f3";
+const imdbUrl = "http://www.omdbapi.com/?apikey=" + apiKey + "&t=";
+
 window.onload = function() {
     showHomePage();
     document.getElementById('home').addEventListener('click', showHomePage);
     document.getElementById('ajaxExample').addEventListener('click', showAjaxExamplePage);
+    document.getElementById('imdb').addEventListener('click', showImdbPage);
+    getMovieData('Superbad');
 }
 
 function showHomePage() {
@@ -102,6 +107,85 @@ function showAjaxExamplePage() {
     document.getElementById('router-outlet').appendChild(table);
 }
 
+function showImdbPage() {
+        // Remove Dynamic Content if exists
+        removeCurrentDynamicContent();
+
+        // Change the breadcrumb so that the user knows we have changed pages
+        let li = document.createElement('li');
+        li.setAttribute('class', 'breadcrumb-item');
+        li.textContent = "IMDB Movie Search";
+    
+        let allBreadcrumbs = document.getElementsByClassName('breadcrumb-item');
+    
+        // Append this breadcrumb
+        if (allBreadcrumbs.length === 1) {
+            document.getElementById('breadcrumb').appendChild(li);
+        }
+    
+        // Change the value of the Title
+        let title = document.querySelector('title');
+        title.textContent = "SPA - IMDB Movie Search";
+    
+        // Create the base table element
+        let table = document.createElement('table');
+        table.setAttribute('class', 'table');
+    
+        // Create the thead and tbody elements
+        let thead = document.createElement('thead');
+        let tbody = document.createElement('tbody');
+        tbody.setAttribute('id', 'moviesTable');
+    
+        // Set the table header row
+        let theadHeader = document.createElement('tr');
+        let tdTitle = document.createElement('th');
+        tdTitle.textContent = 'Title';
+        let tdRated = document.createElement('th');
+        tdRated.textContent = "Rated";
+        let tdGenre = document.createElement('th');
+        tdGenre.textContent = "Genre";
+        let tdReleased = document.createElement('th');
+        tdReleased.textContent = "Released";
+    
+        // Append those th's to our table header row
+        theadHeader.appendChild(tdTitle);
+        theadHeader.appendChild(tdRated);
+        theadHeader.appendChild(tdGenre);
+        theadHeader.appendChild(tdReleased);
+    
+        // Append out thead tr to our thead
+        thead.appendChild(theadHeader);
+    
+        // Append the thead to our table
+        table.appendChild(thead);
+        table.appendChild(tbody);
+
+        // Create a form
+        let form = document.createElement('form');
+
+        // Create an input
+        let movieTitleInput = document.createElement('input');
+        movieTitleInput.setAttribute('id', "movieTitle");
+
+        // Create a button
+        let movieSubmit = document.createElement('button');
+        movieSubmit.setAttribute('class', 'btn btn-outline-success');
+        movieSubmit.setAttribute('id', 'submit');
+        movieSubmit.textContent = "Submit!";
+        movieSubmit.setAttribute('type', 'button');
+
+        movieSubmit.addEventListener('click', getFormData);
+
+
+        // add the input field and button to the form
+        form.appendChild(movieTitleInput);
+        form.appendChild(movieSubmit);
+    
+        // Set the dynamic content
+        document.getElementById('router-outlet').appendChild(form);
+        document.getElementById('router-outlet').appendChild(table);
+}
+
 function removeCurrentDynamicContent() {
     let dynamicContentContainer = document.getElementById('router-outlet');
     let dynamicContent = document.getElementById('router-outlet').firstChild;
@@ -118,6 +202,7 @@ function removeLastBreadcrumb() {
 
     breadcrumbContainer.removeChild(lastBreadcrumb);
 }
+
 function getTodosData() {
     // Step 1: Instantiate and XMLHttpRequest Object
     let xhr = new XMLHttpRequest();
@@ -135,8 +220,6 @@ function getTodosData() {
 
     // Step 4: Call xhr.send()
     xhr.send();
-
-    
 }
 
 function populateTableData(ajaxObject) {
@@ -145,6 +228,7 @@ function populateTableData(ajaxObject) {
         let tdUserId = document.createElement('td');
         let tdTodoId = document.createElement('td');
         let tdTitle = document.createElement('td');
+        let completed = obj.completed;
 
         tdUserId.textContent = obj.userId;
         tdTodoId.textContent = obj.id;
@@ -154,6 +238,34 @@ function populateTableData(ajaxObject) {
         row.appendChild(tdTodoId);
         row.appendChild(tdTitle);
 
+        if (completed) {
+            row.setAttribute('class', 'bg-success');
+        } else {
+            row.setAttribute('class', 'bg-danger');
+        }
+
         document.getElementById('todosTable').appendChild(row);
     }
+}
+
+function getMovieData(movieTitle) {
+    if (typeof movieTitle === 'string') {
+        let xhr = new XMLHttpRequest();
+
+        xhr.onreadystatechange = function() {
+            if (xhr.readyState === 4 && xhr.status === 200) {
+                // Add the movie data to the table here
+                console.log(xhr.responseText);
+            }
+        }
+
+        xhr.open("GET", imdbUrl + movieTitle);
+
+        xhr.send();
+    }
+}
+
+function getFormData() {
+    let movieTitle = document.getElementById('movieTitle').value;
+    getMovieData(movieTitle);
 }
